e311c56363d4a21caa46b1a2b6128439d494c205
UniMarkit <34177175+UniMarkit@users.noreply.github.com>
2018-02-12 17:41:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var S3Adapter = require('parse-server').S3Adapter;
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -37,7 +38,7 @@ var api = new ParseServer({
 	 publicServerURL: 'https://unimarkit-official.herokuapp.com/parse',
 	/* This will appear in the subject and body of the emails that are sent */
 	 appName: process.env.APP_NAME || "UniMarkit-Official", 
-
+	
 	 emailAdapter: {
 	 	module: '@parse/simple-mailgun-adapter',
 	 	options: {
@@ -48,11 +49,11 @@ var api = new ParseServer({
 	 },
 	
 	//**** File Storage ****//
-	// filesAdapter: new S3Adapter(
-	// 	{
-	// 		directAccess: true
-	// 	}
-	// )
+	 filesAdapter: new S3Adapter(
+	 	{
+	 		directAccess: true
+	 	}
+	 )
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
