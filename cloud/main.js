48d6cdf98c3414d479df5f49ad6e345c0e372b92
Teddy Burns <edward.burns@bc.edu>
2018-03-17 10:50:26 -0400

    Added tentative full text search as 'search'

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,23 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('search', function(req, res) {
+  var query = new Parse.Query("Listings");
+  if(req.params.currentUser !== null) {
+    query.notEqualTo("sellerID", req.params.currentUser);
+  }
+  query.equalTo("buySell", true);
+  query.fullText("itemName", req.params.itemName)
+  query.ascending('$score');
+  query.select('$score');
+  query.find()
+    .then(function(results) {
+      response.success(results);
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+
+
+});
