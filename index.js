b38b86fb35c8d9817db5012c7e39ed1facdb7e92
Teddy Burns <edward.burns@bc.edu>
2018-09-03 08:37:51 -0400

    Final push

--- index.js
+++ index.js
@@ -87,12 +87,11 @@ app.get('/refer/:id', function(req, res) {
 	query.equalTo("userID", req.params.id);
 	console.log(req.params.id);
 	query.first().then((goldStatus) => {
-		console.log(goldStatus);
-		const tmp =  goldStatus.get("totalReferralsMade")
-		console.log(tmp);
-		const totalReferralsMade = tmp === undefined ? 0 : tmp;
-		console.log(totalReferralsMade);
+
+		const totalReferralsMade = goldStatus.get("totalReferralsMade") === undefined ? 0 : tmp;
+		const goldPostsLeft = goldStatus.get("goldPostsLeft") === undefined ? 0 : tmp;
 		goldStatus.set("totalReferralsMade", totalReferralsMade + 1)
+		goldStatus.set("goldPostsLeft", goldPostsLeft + 1)
 		return goldStatus.save();
 	}, (error) => {
 		console.log(error);
